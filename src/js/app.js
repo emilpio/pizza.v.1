39fc1254195ab89f4ccaadb54387523f8cd780cd
emilpio <emilpio@gmail.com>
2022-10-20 15:02:14 +0200

    add tables function

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,80 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+
+initPages: function(){
+const thisApp = this;
+
+thisApp.pages = document.querySelector(select.containerOf.pages).children;
+thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+const idFromHash = window.location.hash.replace("#/", "")
+
+
+
+let pageMachingHash = thisApp.pages[0].id;
+
+for(let page of thisApp.pages){
+	if(page.id == idFromHash)
+	{
+		pageMachingHash = page.id;
+		break
+	}
+}
+
+thisApp.activatePage(pageMachingHash);
+
+for (let link of thisApp.navLinks){
+	link.addEventListener("click", function(event){
+		const clickedElement = this;
+		event.preventDefault();
+		 
+// get page ud from href attribute
+
+const id = clickedElement.getAttribute("href").replace("#", "");
+
+// run this.App.activeatePage with that id
+
+thisApp.activatePage(id);
+// change URK hash
+
+window.location.hash = "#" + id;
+
+	})
+}
+
+
+
+	},
+
+	activatePage: function(pageId){
+const thisApp = this;
+
+// add class active to matchin oages, remove fro non-matching
+
+for(let page of thisApp.pages){
+/*	if(page.id == pageId){
+		page.classList.add(classNames.pages.active)
+	} else {
+		page.classList.remove(classNames.pages.active)
+
+	} */
+	page.classList.toggle(classNames.pages.active, page.id == pageId)
+}
+
+for(let link of thisApp.navLinks){
+	link.classList.toggle(
+		classNames.nav.active,
+		link.getAttribute("href") == "#" + pageId
+	);
+}
+
+},
+
+	
   initMenu: function () {
     const thisApp = this;
     // console.log('this App', thisApp);
@@ -43,6 +115,8 @@ const app = {
 
     thisApp.initData();
     thisApp.initCart();
+	thisApp.initPages();
+	thisApp.initBooking();
 
   },
   initCart: function () {
@@ -56,6 +130,11 @@ const app = {
     thisApp.productList.addEventListener('add-to-cart', function (event) {
       app.cart.add(event.detail.product);
     });
+  },
+  initBooking: function(){
+    const thisApp = this;
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingContainer);
   },
 
   
